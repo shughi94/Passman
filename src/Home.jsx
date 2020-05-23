b9d075a1c2930bab17fda7b89704bb2d5a78cf97
Davide Bucher <>
2020-05-23 12:43:55 +0200

    Added card click function

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,15 +24,22 @@ export default class Home extends Component {
       });
   }
 
+  cardClicked(id) {
+    console.log('clicked card: '+id);
+  }
+
   DisplayCredentials(props) {
     const credentials = props.credentials;
+    const fclick = props.onclick;
     const listItems = credentials.map((cred) =>
-    <Card className="credCard">
-      <Card.Header>{cred.title}</Card.Header>
-      <Card.Body>
-        <Card.Text>{cred.website}</Card.Text>
-      </Card.Body>
-    </Card>
+    <a key={cred.id} className="credCard" onClick={fclick.bind(this,cred.id)} style={{ cursor: "pointer" }}>
+      <Card border="primary" bg="light" >
+        <Card.Header>{cred.title}</Card.Header>
+        <Card.Body>
+          <Card.Text>{cred.website}</Card.Text>
+        </Card.Body>
+      </Card>
+    </a>
     );
     return (
       <div className="credCardDiv">{listItems}</div>
@@ -46,7 +53,7 @@ export default class Home extends Component {
         <Card className="credList">
           <Card.Header>List of credentials</Card.Header>
           <Card.Body>
-            <this.DisplayCredentials credentials={this.state.credentials} />
+            <this.DisplayCredentials credentials={this.state.credentials} onclick={this.cardClicked} />
           </Card.Body>
         </Card>
       </div>
